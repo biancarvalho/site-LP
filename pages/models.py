a52238a78ff1162dff1ea378ebc86e90a4e347f8
Cristiano Junior Larrea Leal <junior.larrea.ids@gmail.com>
2021-11-29 20:50:17 -0300

    Add urls.py em cada aplicacao e add max-length nos models

--- pages/models.py
+++ pages/models.py
@@ -4,11 +4,11 @@ class Aluno(models.Model):
     nome = models.CharField(max_length=80)
     idade = models.IntegerField()
     sexo = models.CharField(max_length=1)
-    curso = models.CharField()
+    curso = models.CharField(max_length=80)
     periodo = models.IntegerField()
-    interesses = models.CharField()
+    interesses = models.CharField(max_length=80)
 
 class Interesses(models.Model):
-    categoria = models.CharField() 
+    categoria = models.CharField(max_length=80) 
     grau = models.IntegerField()
-    nome = models.CharField()
+    nome = models.CharField(max_length=80)
