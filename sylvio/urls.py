76f4a0a055a9b6444c9d23cc26358b51c4ba4e22
sjphelt <sylviojorge2010@hotmail.com>
2021-12-03 20:43:30 -0300

    versão inicial da analise

--- sylvio/urls.py
+++ sylvio/urls.py
@@ -1,3 +1,6 @@
-urlpatterns=[
+from django.urls import path
+from sylvio import views as views
 
+urlpatterns=[
+    path('sylvio/main.html', views.main, name='main'),
 ]
--- sylvio/views.py
+++ sylvio/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+import pandas as pd
+from pages.models import Aluno, Interesses, Aluno_Interesses
 
-# Create your views here.
+
+def main(request):
+    context = {
+        
+    }
+    return render(request, 'sylvio/main.html', context=context)
