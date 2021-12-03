fe627d4eb1c8da223a572eb214f59155d3a8b91a
Cristiano Junior Larrea Leal <junior.larrea.ids@gmail.com>
2021-12-03 09:50:11 -0300

    Add tabela intermediaria em models, iniciei meu app

--- cristiano/views.py
+++ cristiano/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+import pandas as pd
+from pages.models import Aluno, Interesses, Aluno_Interesses
 
-# Create your views here.
+alunos = Aluno.objects
+interesses = Interesses.objects
+alunos_interesses = Interesses.objects
+
+lista_alunos = alunos.values()
+lista_interesses = interesses.values()
+lista_alunos_interesses = alunos_interesses.values()
+
+interesses_distintos = interesses.values('nome').distinct()
+interesses_saudaveis = ['Dança',
+'Volei', 'Futebol', 'Handbol', 'Basquete',
+'Tenis de Mesa', 'Natação', 'Crossfit', 'Musculação',
+'Golfe', 'Passeio no Parque', 'Trilhas', 'Montanhas']
+
+
+alunos_df = pd.DataFrame(lista_alunos)
+interesses_df = pd.DataFrame(lista_interesses)
+alunos_interesses_df = pd.DataFrame(lista_alunos_interesses)
+
+#alunos_df = alunos_df.merge(alunos_interesses_df, left_on='id', right_on='aluno_id_id')
+
+
+
+
+def eda(request):
+    context = {
+        'alunos': lista_alunos[0:20],
+        'interesses': lista_interesses[0:20],
+        'interesses_distinct': interesses_distintos,
+        'interesses_saudaveis': interesses_saudaveis,
+        #'aluno_df': alunos_df
+    }
+    return render(request, 'cristiano/eda.html', context=context)
