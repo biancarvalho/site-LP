bd28a152d049e67bdef7bad215a71b30a3f0ac98
Raul Figueiredo <raullomonte13@gmail.com>
2021-12-05 13:38:45 -0300

    corrigindo analise

--- daniel/views.py
+++ daniel/views.py
@@ -24,13 +24,13 @@ alunos_interesses = pd.DataFrame(alunos_interesses)
 merge_inicial = alunos.merge(alunos_interesses, left_on='id', right_on='aluno_id_id').merge(interesses, left_on='interesses_id_id', right_on="id")
 merge_inicial = merge_inicial.rename(columns={'id_x': 'id_aluno', 'nome_x': 'nome_aluno', 'id_y':'id_interesse','id_y':'id_aluno_interesse','id':'id_interesse','nome_y':'nome_interesse'})
 
-data = merge_inicial[['id_interesse','categoria','nome_interesse','grau']]
+data = merge_inicial[['curso','id_interesse','categoria','nome_interesse','grau']]
 
 
 
 ###Analises
 #fazendo a tabela relacionado idade com os interesses
-data = data[['nome_interesse','grau']]
+data = data[['curso','nome_interesse','grau']]
 data_soma_interesse= data.groupby(['nome_interesse']).sum().sort_values(['grau'],ascending=False)
 
 data_soma_interesse_graf=data_soma_interesse.sort_values('grau')
@@ -70,13 +70,13 @@ MAT_gb_html = re.sub("class=\"dataframe ", "class=\"", MAT_gb.head(5).to_html(cl
 CD_MAT_html = re.sub("class=\"dataframe ", "class=\"", CD_MAT.head(5).to_html(classes='table table-striped',justify='left'))
 
 ## Graficos 
-MAT_gb_graf.plot(kind='barh',figsize=(15,12), color="orange")
+MAT_gb_graf.plot(kind='barh',figsize=(9,12), color="orange")
 plt.xlabel('Nivel de interesse')
 plt.ylabel('Interesses')
 plt.title('Visão geral dos interesses dos alunos de MAp', loc='left')
 plt.legend().remove()
 buffer = BytesIO()
-plt.savefig(buffer, format='png')
+plt.savefig(buffer, format='png', bbox_inches='tight')
 buffer.seek(0)
 image_png = buffer.getvalue()
 buffer.close()
@@ -87,13 +87,13 @@ MAT_gb_graf = MAT_gb_graf.decode('utf-8')
 
 
 
-CD_gb_graf.plot(kind='barh',figsize=(15,12), color="blue")
+CD_gb_graf.plot(kind='barh',figsize=(9,12), color="blue")
 plt.xlabel('Nivel de interesse')
 plt.ylabel('Interesses')
 plt.title('Visão geral dos interesses dos alunos de CD', loc='left')
 plt.legend().remove()
 buffer = BytesIO()
-plt.savefig(buffer, format='png')
+plt.savefig(buffer, format='png', bbox_inches='tight')
 buffer.seek(0)
 image_png = buffer.getvalue()
 buffer.close()
@@ -103,13 +103,13 @@ CD_gb_graf = CD_gb_graf.decode('utf-8')
 
 
 
-data_soma_interesse_graf.plot(kind='barh',figsize=(15,15), color="turquoise")
+data_soma_interesse_graf.plot(kind='barh',figsize=(9,15), color="turquoise")
 plt.xlabel('Nivel de interesse')
 plt.ylabel('Interesses')
 plt.title('Visão geral dos interesses', loc='left')
 plt.legend().remove()
 buffer = BytesIO()
-plt.savefig(buffer, format='png')
+plt.savefig(buffer, format='png', bbox_inches='tight')
 buffer.seek(0)
 image_png = buffer.getvalue()
 buffer.close()
@@ -117,13 +117,13 @@ data_soma_interesse_graf = base64.b64encode(image_png)
 data_soma_interesse_graf = data_soma_interesse_graf.decode('utf-8')
 
 
-CD_MAT_graf[['grau_CD','grau_MAT']].plot(figsize=(15,12),kind='barh')
+CD_MAT_graf[['grau_CD','grau_MAT']].plot(figsize=(9,12),kind='barh')
 plt.xlabel('Nivel de interesse')
 plt.ylabel('Interesses')
 plt.title('Visão geral dos interesses dos alunos de CD', loc='left')
 plt.legend()
 buffer = BytesIO()
-plt.savefig(buffer, format='png')
+plt.savefig(buffer, format='png', bbox_inches='tight')
 buffer.seek(0)
 image_png = buffer.getvalue()
 buffer.close()
